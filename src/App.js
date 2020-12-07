757b50a5a7d37c272a056920718af402cef42ae6
Movsar <movsar1991.mb@gmail.com>
2020-12-07 16:07:08 +0300

    mmm123

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+function App(props) {
+  const st = useSelector((state)=>{
+    return state;
+  });
+  const dispatch = useDispatch();
 
-function App() {
+  const handleOdin = () =>{
+    dispatch({type: 'odin'})
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+     Слово из стейта: {st}  
     </div>
+    <div>
+      <button onClick={handleOdin}>
+        Один
+      </button>
+      <button onClick={()=>dispatch({type: 'dva'})}>
+        Два
+      </button>
+      <button onClick={()=>dispatch({type: 'tri'})}>
+        Три
+      </button>
+    </div>
+    <div><button onClick={()=>dispatch({type: 'reset'})}> reset </button> </div>
+    </div>
+    
   );
 }
 
